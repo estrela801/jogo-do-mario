d899a90fa9ead42ab5ea1e5d532488a4606e81cb
Rodrigo Henrique Fonseca Estrela <rohenri0808@gmail.com>
2024-04-14 23:50:20 -0300

    Jogo do mario criado :)

--- script.js
+++ script.js
@@ -8,16 +8,40 @@ document.addEventListener('click', () => {
     document.querySelector('.mario').classList.add('jump')
     setTimeout(()=>{
     document.querySelector('.mario').classList.remove('jump')
-    },  1000 )
+    },  800 )
+})
+document.addEventListener('keydown', () => {
+    document.querySelector('.mario').classList.add('jump')
+    setTimeout(()=>{
+    document.querySelector('.mario').classList.remove('jump')
+    }, 800 )
 })
 
 const loop = setInterval(() => {
-    const pipePosition = pipe.offsetLeft();
-    console.log(pipePosition)
+    let pipePosition = document.querySelector('.pipe').offsetLeft;
+    const mario = document.querySelector('.mario');
+    let marioPosition = mario
+    
+    marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
+    
+    console.log(marioPosition)
      
-    if(pipePosition <= 120){
-        pipe.style.animation = 'none'
+    if(pipePosition <= 150 && pipePosition > 0 && marioPosition < 100 ){
+        document.querySelector('.pipe').style.animation = 'none'
+        document.querySelector('.pipe').style.left = `${pipePosition}px`
+
+        document.querySelector('.mario').style.animation = 'none'
+        document.querySelector('.mario').style.bottom = `${marioPosition}px`
+
+        document.querySelector('.mario').src = 'imagens/game-over.png';
+        document.querySelector('.mario').style.width = '60px'
+        document.querySelector('.mario').style.marginLeft = '50px'
+
+        clearInterval(loop)
+        document.querySelector('#button').style.display = 'block'
+
     }
+    
 
 }, 10)
 
